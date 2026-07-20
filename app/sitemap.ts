268266c3f2f36a328c54c43b6a73ae0a71cdb8db
reyjohnlorca3 <reyjohnlorca3@gmail.com>
2026-07-20 21:21:35 +0800

    Make metadata routes exportable

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [{ url: "https://www.lorcatecture.com", lastModified: new Date(), changeFrequency: "monthly", priority: 1 }];
 }
